651986de0475f5addd2ded338fe7c5884f61d709
Rexphel <philiprexroth@gmail.com>
2024-10-28 20:34:43 +0100

    Init

---
+++ versuch_2_rgb/Core/Src/rgb.h
@@ -0,0 +1,19 @@
+/*
+ * rgb.h
+ *
+ *  Created on: Oct 23, 2024
+ *      Author: rexphel
+ */
+
+#ifndef SRC_RGB_H_
+#define SRC_RGB_H_
+
+struct rgb {
+	int red;
+	int green;
+	int blue;
+};
+
+typedef struct rgb RGB;
+
+#endif /* SRC_RGB_H_ */
